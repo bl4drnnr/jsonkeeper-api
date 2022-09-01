d0f758d289594ca9832950d1f65a4cb26e2b909f
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-09-01 20:32:20 +0200

    basic modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { CollectionModule } from './modules/collection/collection.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, CollectionModule],
   controllers: [AppController],
   providers: [AppService],
 })
