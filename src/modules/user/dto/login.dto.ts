9c057624805a9028f76cd6ec7867704ab17fe48d
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-09-02 17:43:16 +0200

    class validator and transformer

--- src/modules/user/dto/login.dto.ts
+++ src/modules/user/dto/login.dto.ts
@@ -1,3 +1,13 @@
+import { IsEmail, IsNotEmpty, IsString, Length } from "class-validator";
+
 export class LoginDto {
-  //
+  @IsEmail()
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(6, 32)
+  password: string;
 }
--- src/modules/user/dto/register.dto.ts
+++ src/modules/user/dto/register.dto.ts
@@ -1,3 +1,23 @@
+import { IsEmail, IsNotEmpty, IsString, Length } from "class-validator";
+
 export class RegisterDto {
-  //
+  @IsString()
+  @IsNotEmpty()
+  @Length(2, 32)
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(2, 32)
+  lastName: string;
+
+  @IsEmail()
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(6, 32)
+  password: string;
 }
