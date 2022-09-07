ae2c56e7e251f4b846b86b0bb54651053c1fcccf
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-09-07 18:14:15 +0200

    global config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,20 @@ import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { CollectionModule } from './modules/collection/collection.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { SharedModule } from './shared/shared.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, CollectionModule, PrismaModule]
+  imports: [
+    AuthModule,
+    UserModule,
+    CollectionModule,
+    PrismaModule,
+    SharedModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env`
+    })
+  ]
 })
 export class AppModule {}
